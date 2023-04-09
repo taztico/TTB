2a24b94dd1e1aa71da600d52312266ea55a60a04
taztico <tomas.campusano11@gmail.com>
2023-04-09 19:42:35 -0400

    Archivo JSON

--- tt.py
+++ tt.py
@@ -1,28 +1,34 @@
 import tweepy
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.tz import gettz
 import time
+import json
 
+contenido= open("info.json")
+config = json.load(contenido)
+estado = config["status"]
 
 api_key = ''
 api_secret= ''
 bearer_token = f''
 access_token = ''
 access_token_secret = ''
+    
 
 client = tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret)
 auth = tweepy.OAuthHandler(api_key, api_secret,access_token,access_token_secret)
 api = tweepy.API(auth)
 
 def tweet():
+    fecha = date(2023, 3, 10) # fecha en formato año-mes-día
+    hoy = date.today() # hoy
+    dif = hoy-fecha
+    dias_pasados = dif.days
     now = datetime.now(gettz('GMT-4'))
     current_time = now.strftime('%H:%M')
 
-    message = "NO. Hora: {}".format(current_time)
+    message = "{}. Hora: {} Dias sin la capsula {}".format(estado,current_time,dias_pasados)
     api.update_status(message)
     print("Tuit enviado!")
 
-
-while True:
-    tweet()
-    time.sleep(1800)
+tweet()
